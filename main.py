9cc275e5ab4931f60dd7d635ea685dd47c17e712
izhartecnicion-prog <izhartecnicion@gmail.com>
2026-09-09 18:23:10 +0500

    Update firmware URL to new release version

--- main.py
+++ main.py
@@ -481,7 +481,7 @@ async def admin_reset_all_device_data(device_id: str, request: Request):
 def check_firmware_update():
     return {
         "latest_version": "v1.19",  # Jab bhi naya update release karna ho, yahan version change kar dein
-        "firmware_url": "https://raw.githubusercontent.com/your-username/your-repo/main/firmware.bin" # GitHub raw .bin link
+        "firmware_url": "https://github.com/izhartecnicion-prog/video-downloader-backend/releases/download/v1.20/TEST_TIGER.ino.bin" # GitHub raw .bin link
     }
 
 
